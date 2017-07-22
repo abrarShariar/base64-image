03c720f73110c40a7129c7bf47f0c63bbe0af7ca
abrarShariar <shariarabrar@gmail.com>
2017-07-22 20:07:29 +0600

    regex modified to accept all file types and gitignore update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function base64ToImage(dir) {
     var raw = req.body.base64;
     var filename = req.params.filename ? req.params.filename : 'demo.png';
     if (!checkBase64(raw)) return next();
-    var base64 = raw.replace(/^data:image\/png;base64,/, "");
+    var base64 = raw.replace(/^data:image\/.;base64,/, "");
     var abs = path.join(dir, filename);
     fs.writeFile(abs, base64, 'base64', function(err) {
       if (err) return next(err);
@@ -22,5 +22,5 @@ function base64ToImage(dir) {
 }
 
 function checkBase64(raw) {
-  return raw && typeof(raw) == 'string' && raw.match(/^data:image\/png;base64,/)
+  return raw && typeof(raw) == 'string' && raw.match(/^data:image\/.;base64,/)
 }
